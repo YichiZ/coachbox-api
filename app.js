4ab115c073957bc41ce0882fdca3d0a23f0eec03
Yichi Zhang <yuchiras.zhang@gmail.com>
2020-05-27 19:27:41 -0400

    Fix body-parser

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('MongoDB connected'))
     .catch(console.log);
 
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', require('./routes/index'));
-//app.use('/users', require('./routes/users'));
+app.use('/api/users', require('./routes/users'));
 
 app.listen(process.env.PORT, () => console.log(`Server starter on port ${process.env.PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,9 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => res.send('Hello World from User'));
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
 module.exports = router;
