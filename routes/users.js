aa20e9b1503f31bfc090c29ec99d3e695db0cb2d
Yichi Zhang <yuchiras.zhang@gmail.com>
2020-05-28 21:41:41 -0400

    Simplify code more

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,34 @@
 const router = require('express').Router();
-const usersController = require('../controllers/usersController');
+const User = require('../models/User');
 
-router.get('/', usersController.getItems);
+router.get('/', async (req, res) => {
+    const users = await User.find()
+        .exec()
+        .then()
 
-router.get('/:id', usersController.getItem);
+    res.status(200).json(users);
+});
 
-router.post('/', usersController.createItem);
+router.get('/:id', async (req, res) => {
+    const email = req.params['id'];
+
+    const user = await User.findOne({email})
+        .exec()
+        .catch(err => console.log(err));
+
+    user ? res.status(200).json(user) : res.sendStatus(404);    
+});
+
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email
+    });
+
+    const response = await user.save()
+        .catch(err => console.log(err));
+
+    res.status(201).json(response);
+});
 
 module.exports = router;
