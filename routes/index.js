83fe8dfc54a935314c28044ddc933c36f25529ea
Yichi Zhang <yuchiras.zhang@gmail.com>
2020-06-09 17:57:27 -0400

    Add Authorize middleware

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const Authorize = require('../middleware/Authorize');
 
 router.get('/', (req, res) => res.send('This api is running'));
+
+router.get('/isAuthorized', Authorize, (req, res) => res.send('This api is authorized'));
 
 module.exports = router;
